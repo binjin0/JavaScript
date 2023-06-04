8067152071a48323a0664a4a0ad19d15c54d2e9c
binjin0 <a032875@gmail.com>
2023-06-04 13:52:39 +0900

    2.3.1.변수명 짓기

---
+++ 2.3.변수/2.3.1.변수명짓기.js
@@ -0,0 +1,9 @@
+/*변수명 짓기
+1. 변수명은 저장된 값이 무엇인지 추측할 수 있도록 짓는 것이 best이다.
+2. 변수명은 특수문자 $와_를 사용할 수 있으며 숫자로 시작해서는 안된다. 
+3. 변수명은 영어로 짓는 것이 좋다.
+4. 예약어는 변수명으로 사용할 수 없다.
+ex) await, break, case, catch, class, const, continue, debugger, default, delete, do, else, enum,
+export, extends, false, finally, for, function, if, import, in, instanceof, new, null, return, super, switch, 
+this, throw, true, try, typeof, var, void, while, with, yield*/
+//let let='no!'이러면 오류가 생기겠쥬?
