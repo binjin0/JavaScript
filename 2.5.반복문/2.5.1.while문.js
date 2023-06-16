7a21364447df1c5f5fb74a14939ac686f1496694
binjin0 <a032875@gmail.com>
2023-06-16 23:29:03 +0900

    2.5.1

---
+++ 2.5.반복문/2.5.1.while문.js
@@ -0,0 +1,11 @@
+//while문 : 조건식이 참인 동안 반복해서 실행문을 실행한다.
+/*실행문이 하나라도 괄호로 묶어주는 것이 좋다
+while(조건식){
+     실행문;} */
+
+//hellow while! 100번 출력하기
+let i = 0;
+while (i < 100) {
+  console.log("hellow while!");
+  i++;
+}
