417dfa28b0665e504a852e4193efef7d38a4e830
binjin0 <a032875@gmail.com>
2023-06-18 00:22:37 +0900

    5장 끝

--- 2.5.반복문/2.5.6 중첩 반복문.js
+++ 2.5.반복문/2.5.6 중첩 반복문.js
@@ -1,6 +1,14 @@
 //중첩반복문 : 반복문 안에 반복문이 들어있는 경우
+// for (let i = 0; i < 10; i++) {
+//   for (let j = 0; j < 10; j++) {
+//     console.log(i, j);
+//   }
+// }
+//구구단을 출력하되, 결과에 짝수가 하나도 나오지 않게 해보세요.continue문을 사용하여
 for (let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
-    console.log(i, j);
+    if ((i * j) % 2 != 0) {
+      console.log(i + "*" + j + "=" + i * j);
+    }
   }
 }
