c7ffbb76422c2479ced43c1c933dd40478aec586
binjin0 <a032875@gmail.com>
2023-06-20 15:49:48 +0900

    2.3.4

--- 2.3.변수/2.3.4.상수 만들기.js
+++ 2.3.변수/2.3.4.상수 만들기.js
@@ -1,4 +1,5 @@
 //상수 : 변하지 않는 수
+//상수는 cosnt로 표현 그러나 cosnt는 꼭 상수는 아님. const에 객체(배열,함수,객체 리터럴)가 대입되면 객체 내부의 속성이나 배열의 요소는 수정할 수 있다.
 const value = "상수입니다.";
 value = "바꿀 수 없습니다.";
 console.log(value); //error
